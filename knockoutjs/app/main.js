cae72721dae4a06ef9a02ba5cfeb3991eb008b94
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2013-09-10 08:45:35 +0200

    cleaning knockout

--- knockoutjs/app/main.js
+++
@@ -1,9 +0,0 @@
-// to depend on a bower installed component:
-// define(['component/componentName/file'])
-
-define(["jquery", "knockout"], function($, ko) {
-  var viewModel = {
-    status: ko.observable('active')
-  };
-  ko.applyBindings(viewModel, $('html')[0]);
-});
